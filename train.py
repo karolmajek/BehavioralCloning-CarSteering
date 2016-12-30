5221237df5e2c4f5852b8456ad2ab8c6812f9891
Karol Majek <karolmajek@gmail.com>
2016-12-30 16:41:32 +0100

    loading with tqdm; corrected output vec

--- train.py
+++ train.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 import pickle
 import tensorflow as tf
 # TODO: import Keras layers you need here
@@ -6,31 +7,50 @@ from keras.layers import Input, Flatten, Dense
 from keras.models import Model
 import csv
 import cv2
+from tqdm import tqdm
 
 flags = tf.app.flags
 FLAGS = flags.FLAGS
 
 # command line flags
-flags.DEFINE_string('training_dir', 'recordings/track0_trail0', "Directory with training data from simulator")
-flags.DEFINE_string('validation_dir', 'recordings/track0_trail0', "Directory with validation data from simulator")
+flags.DEFINE_string('training_dir', 'recordings/track0_trail1', "Directory with training data from simulator")
+flags.DEFINE_string('validation_dir', 'recordings/track0_trail1', "Directory with validation data from simulator")
 flags.DEFINE_integer('epochs', 50, "The number of epochs.")
 flags.DEFINE_integer('batch_size', 256, "The batch size.")
+flags.DEFINE_integer('show_input', False, "Input data visualization")
 
+image_labels=['Center image','Left image','Right image']
+data_labels=['Steering Angle','Throttle','Break','Speed']
+
+def normalize(data):
+    return 0.8*(data-data.min())/(data.max()-data.min())+0.1
 
 def load_data(training_dir, validation_dir=None):
     if validation_dir == None:
         validation_dir=training_dir
-
+    X_train=[]
+    y_train=[]
     with open(training_dir+'/driving_log.csv', 'r') as csvfile:
-        data = csv.reader(csvfile, delimiter=',')
-        for l in data:
+        data = list(csv.reader(csvfile, delimiter=','))
+        pbar = tqdm(range(len(data)), desc='Loading', unit='images')
+        for i in pbar:
+            l=data[i]
             image0=cv2.imread(l[0].lstrip())
             image1=cv2.imread(l[1].lstrip())
             image2=cv2.imread(l[2].lstrip())
             image=np.concatenate((image1,image0,image2),axis=1)
-            cv2.imshow('cameras',image)
-            cv2.waitKey(1)
-            print(l[3:])
+            if FLAGS.show_input:
+                cv2.imshow('cameras',image)
+                cv2.waitKey(1)
+            # Lets work with center image only
+            X_train.append(normalize(image0))
+            output=float(l[3])
+            y_train.append(output)
+    X_train=np.array(X_train)
+    y_train=np.array(y_train)
+
+    print(X_train.shape)
+    print(y_train.shape,np.min(y_train),np.max(y_train))
     return #X_train, y_train, X_val, y_val
 
 def main(_):
